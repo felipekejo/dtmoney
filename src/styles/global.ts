de57f5fea00b74708a9668974d6b997a2c4f3e0f
Felipe Yui <felipe_m87@yahoo.com.br>
2021-06-15 14:34:30 +0930

    google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -29,8 +29,17 @@ html{
 
 body{
   background:var(---background);
-  -webkit-font-smoothing:antialiased
+  -webkit-font-smoothing:antialiased;
 }
+body, input, textarea, button{
+  font-family:'Poppins', sans-serif;
+  font-weight:400;
+}
+
+h1, h2,h3,h4,h5,h6, strong{
+  font-weight:600;
+}
+
 button{
   cursor:pointer;
 }
