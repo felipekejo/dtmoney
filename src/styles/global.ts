ba22014b4b4b931baf413c9efd2e9f87dcf33edd
Felipe Yui <felipe_m87@yahoo.com.br>
2021-06-15 14:29:49 +0930

    global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+  --background:#f0f2f5;
+  --blue:#5429CC;
+  --blue-light:#6933FF;
+  --red:#E62E4D;
+  --green:#33CC95;
+  --shape:#FFFFFF;
+  --text-title:#363F5F;
+  --text-body:#969CB3;
+}
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+
+html{
+  @media(max-width:1000px){
+    font-size:93.75%;
+  }
+  @media(max-width:720px){
+    font-size:87.5%;
+  }
+}
+
+body{
+  background:var(---background);
+  -webkit-font-smoothing:antialiased
+}
+button{
+  cursor:pointer;
+}
+
+[disabled]{
+  opacity:0.6;
+  cursor:not-allowed
+}
+
+`
