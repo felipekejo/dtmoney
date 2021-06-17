fb5ef4affa5b5f17665e4a7cc42313965f4b5e3f
Felipe Yui <felipe_m87@yahoo.com.br>
2021-06-17 14:34:54 +0930

    creating Transaction on API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -21,6 +22,13 @@ export function NewTransactionModal({
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+    api.post('transactions', data);
   }
 
   return (
